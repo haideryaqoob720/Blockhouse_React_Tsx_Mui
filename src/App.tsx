ffb30e51c7277c8d4c38cd9c290dd8613fcfd21d
Haider Yaqoob <49457040+haideryaqoob720@users.noreply.github.com>
2023-02-17 16:36:33 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+// Components
+import BlockhouseLiquidity from "./components/BlockhouseLiquidity";
+import BlockhouseStructured from "./components/BlockhouseStructured";
+
+// CSS
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* We have to hide routes because github free hosting did not accept this */}
+      {/* <BlockhouseLiquidity /> */}
+      <BlockhouseStructured />
+
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BlockhouseLiquidity />} />
+          <Route path="/BlockhouseStructured" element={<BlockhouseStructured />} />
+        </Routes>
+      </BrowserRouter> */}
+    </>
+  );
+}
+
+export default App;
