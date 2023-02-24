81997496ccbb85c3dbd33c1ef7964b43986f4ae9
Haider Yaqoob <49457040+haideryaqoob720@users.noreply.github.com>
2023-02-24 17:02:46 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+// Components
+import LandingPage from "./components/LandingPage";
+import BlockhouseLiquidity from "./components/BlockhouseLiquidity";
+import BlockhouseStructured from "./components/BlockhouseStructured";
+import BorrowPage from "./components/BorrowPage";
+import CommingSoon from "./components/CommingSoon";
+import ThankYou from "./components/ThankYou";
+
+// CSS
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* We have to hide routes because github free hosting did */}
+      {/* <BlockhouseLiquidity /> */}
+      {/* <BlockhouseStructured /> */}
+      {/* <LandingPage /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/BlockhouseLiquidity"
+            element={<BlockhouseLiquidity />}
+          />
+          <Route
+            path="/BlockhouseStructured"
+            element={<BlockhouseStructured />}
+          />
+          <Route path="/BorrowPage" element={<BorrowPage />} />
+          <Route path="/CommingSoon" element={<CommingSoon />} />
+          <Route path="/ThankYou" element={<ThankYou />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ThankYou.tsx
@@ -0,0 +1,56 @@
+import {
+  HeroBtn,
+  HeroBtnBorder,
+  AccessContainer,
+  FooterButtonBox,
+  HeroHeading,
+  TopContainer,
+} from "./landingPageComponents/CustomComponents";
+import { Box, Typography } from "@mui/material";
+
+// icons
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+
+// Components
+import LandingPageNavbar from "./landingPageNavbar";
+// import Footer from "./landingPageComponents/Footer";
+
+// Images
+import borrowPageBackground from "../Images/borrowPage/borrowPageBackground.jpg";
+
+function AccessSection() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "none",
+        background: `url(${borrowPageBackground})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      {/* // <Box sx={{ backgroundColor: 'none'}}> */}
+      <TopContainer sx={{ background: "none !important" }}>
+        {/*---------- navbar -----------*/}
+        <LandingPageNavbar />
+
+        <AccessContainer sx={{ background: "none", width: "80%" }}>
+          {/* <HeroHeading variant="h1" sx={{ mt: "250px", marginBottom: "72px" }}> */}
+          <HeroHeading
+            variant="h1"
+            sx={{
+              lineHeight: "40px !important",
+              fontSize: "36px !important",
+              letterSpacing: "-1px",
+              mt: { xl: "280px", md: "180px", sm: "40ox" },
+            }}
+          >
+            Thank you for signing up!
+          </HeroHeading>
+          {/* <Typography variant="h6" color={"white"} sx={{mt: '72px'}}> */}
+        </AccessContainer>
+      </TopContainer>
+    </Box>
+  );
+}
+
+export default AccessSection;
